a4893d0fc07ab83d1192dbb98a174248ada72545
GitBitWheat <67285289+GitBitWheat@users.noreply.github.com>
2020-08-15 21:52:40 +0300

    i luv u

--- src/Matrix.c
+++ src/Matrix.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdint.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -231,76 +230,4 @@ ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar) {
 	}
 
 	return ERROR_SUCCESS;
-=======
-#include "Matrix.h"
-
-struct Matrix {
-    void* impl;
-    // Empty implementation
-};
-
-ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width) {
-    (void)matrix;
-    (void)height;
-    (void)width;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_copy(PMatrix* result, CPMatrix source) {
-    (void)result;
-    (void)source;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-void matrix_destroy(PMatrix matrix) { (void)matrix; }
-
-ErrorCode matrix_getHeight(CPMatrix matrix, uint32_t* result) {
-    (void)matrix;
-    (void)result;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_getWidth(CPMatrix matrix, uint32_t* result) {
-    (void)matrix;
-    (void)result;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_setValue(PMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
-                          double value) {
-    (void)matrix;
-    (void)rowIndex;
-    (void)colIndex;
-    (void)value;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_getValue(CPMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
-                          double* value) {
-    (void)matrix;
-    (void)rowIndex;
-    (void)colIndex;
-    (void)value;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_add(PMatrix* result, CPMatrix lhs, CPMatrix rhs) {
-    (void)result;
-    (void)lhs;
-    (void)rhs;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs) {
-    (void)result;
-    (void)lhs;
-    (void)rhs;
-    return ERROR_NOT_IMPLEMENTED;
-}
-
-ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar) {
-    (void)matrix;
-    (void)scalar;
-    return ERROR_NOT_IMPLEMENTED;
->>>>>>> aee745b52d52ae6154f8bd82e5c95bb93675b207
 }
